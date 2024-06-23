fb14cb1ff2c903706930360655edb53dc3ae84a5
sturec5 <seda@turec.net>
2024-06-23 21:05:43 +0300

    dfsdf

--- components/nftBox.js
+++ components/nftBox.js
@@ -1,14 +1,18 @@
 export default function NFTBox({ name, image, description, attributes }) {
     return (
-        <div class="max-w-sm bg-white border border-zinc-200 rounded-lg shadow dark:bg-zinc-800 dark:border-zinc-700">
-            <a href="#">
-                <img class="rounded-t-lg" src={image} alt={`NFT ${name}`} />
-            </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-zinc-900 dark:text-white">{name}</h5>
-                </a>
-                <p class="mb-3 font-normal text-zinc-700 dark:text-zinc-400">{description}</p>
+        <div className="max-w-sm bg-white border border-zinc-200 shadow dark:bg-zinc-800 dark:border-zinc-700">
+            <div>
+                <img src={image} alt={`NFT ${name}`} />
+            </div>
+            <div className="p-5">
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-zinc-900 dark:text-white tracking-widest">{name}</h5>
+                <p className="text-sm mb-4 font-normal text-zinc-700 dark:text-zinc-400">{description}</p>
+                <h6 className="mb-1 text-l font-bold tracking-tight text-zinc-900 dark:text-white tracking-widest">Attributes:</h6>
+                <ul className="list-inside list-disc text-sm font-semibold mb-2 font-normal text-zinc-700 dark:text-zinc-300 leading-6">
+                    <li>Palette: Blahblah</li>
+                    <li>Branch Length: Blahblah </li>
+                    <li>Depth: Blahblah</li>
+                </ul>
             </div>
         </div>
     )
